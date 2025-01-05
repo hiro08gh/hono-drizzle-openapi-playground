cd9ff9fa573b7d2b49af518c955383a0df1e8716
hiro08gh <hiro08gh@gmail.com>
2025-01-05 15:57:33 +0900

    first commit

---
+++ src/schema.ts
@@ -0,0 +1,10 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable(
+  "users",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name").notNull(),
+  },
+  () => []
+);
