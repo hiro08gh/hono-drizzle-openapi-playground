dc0efe635e494307f749bbf06a169d951c3674cd
hiro08gh <hiro08gh@gmail.com>
2025-01-05 20:21:33 +0900

    add: todos

--- src/schema.ts
+++ src/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable(
@@ -8,3 +9,15 @@ export const users = sqliteTable(
   },
   () => []
 );
+
+export const todos = sqliteTable("todos", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  status: text("status", { enum: ["todo", "doing", "done"] }).default("todo"),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`
+  ),
+});
