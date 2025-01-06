3f75b2c1b94ab55ae58164afec200709de32efaa
hiro08gh <hiro08gh@gmail.com>
2025-01-06 15:46:55 +0900

    chore: move zod schema

--- src/routes/hello/index.ts
+++ src/routes/hello/index.ts
@@ -1,8 +1,24 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { createRoute } from "@hono/zod-openapi";
 
-import { route } from "./schema";
+import { HelloSchema } from "./schema";
 
 const app = new OpenAPIHono();
+
+export const route = createRoute({
+  path: "/",
+  method: "get",
+  responses: {
+    200: {
+      description: "hello entrypoint",
+      content: {
+        "application/json": {
+          schema: HelloSchema,
+        },
+      },
+    },
+  },
+});
 
 app.openapi(route, (c) => {
   return c.json({ message: "Hello Hono!" });
