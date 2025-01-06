45af7593ec516c8e23812e3604c1c53de26ab8c3
hiro08gh <hiro08gh@gmail.com>
2025-01-06 15:49:59 +0900

    chore: change test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -17,17 +17,6 @@ describe("hello test", () => {
   });
   it("正常系 /", async () => {
     const res = await app.request(
-      "/",
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      },
-      env
-    );
-    expect(res.status).toBe(200);
-  });
-  it("正常系 /todos", async () => {
-    const res = await app.request(
       "/hello",
       {
         method: "GET",
